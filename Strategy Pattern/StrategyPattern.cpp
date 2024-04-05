fa5b84ef300322cccfdf16f7a2199d3de8424fb0
Ankith Reddy Adudodla <ankithreddy@Ankiths-MacBook-Air.local>
2024-04-05 19:38:38 -0400

    Added SOLID Explanations

--- Strategy Pattern/StrategyPattern.cpp
+++ Strategy Pattern/StrategyPattern.cpp
@@ -3,6 +3,40 @@
 #include "FlyWithLegs.hpp"
 using namespace std;
 
+
+/*
+
+    Follows SOLID Principles"
+
+    1. Single Responsibility Principle:
+        A class should have one, and only one reason to change.
+        Example: 
+            Fly Interface - Has only one responsibility that is Fly.
+
+    2. Open Closed Principle:
+        Example: 
+            Open for Extension: You can add more Fly Behaviors
+            Closed for Modification: There is no requirement of modifying Duck Class.
+
+    3. Liskov Substitution Principle:
+        Objects of superclass should be able to replace with subclass.
+        Example: 
+            Like, Duck * duck = new MallardDuck();
+
+    4. Interface Segregation Principle:
+        No client should be forced to implement methods/behaviors that are not required
+        Example:
+            ----
+
+    5. Dependency Inversion Principle:
+        A class shouldn't depend on concrete entities rather it should depend on **abstract** entities.
+        Example:
+            FlyInterface * fi in Duck
+
+
+
+*/
+
 class Duck {
     private:
     FlyInterface* fi;
@@ -18,8 +52,20 @@ class MallardDuck : public Duck {
     MallardDuck() : Duck(new FlyWithLegs()) {}
 };
 
+class RubberDuck : public Duck {
+    public:
+    RubberDuck() : Duck(new FlyWithWings()) {}
+};
+
+class RandomDuck : public Duck {
+    public:
+    RandomDuck() : Duck(new FlyWithLegs()) {}
+};
+
+
+
 int main() {
-    MallardDuck* md = new MallardDuck();
+    Duck* md = new MallardDuck();
 
     md->performFly();
 }
